cd42a37e496583735b003a8f1b23a34d3ec3a0d5
thatguykwameC <thatguykwamec@gmail.com>
2026-06-04 17:34:37 -0400

    Restructured flow

--- Hangman/app.py
+++ Hangman/app.py
@@ -1,31 +1,33 @@
-from utils import welcome_message, get_user_input, display_output, lives_lost
-from config import HANGMANPICS, word, display
+from config import word, display
+from utils import welcome_message, get_user_input
+from utils import lives_lost, display_output
+from utils import win_loose
 
 
 def main():
-    """"""
+    """the main app"""
+
+    life_count = 7
+    count = 0
     active = True
+
     welcome_message()
 
     while active:
-
         guess = get_user_input()
 
-        life_count = lives_lost(guess)
-
-    
+        life_count = lives_lost(guess, life_count, count)
 
         if life_count == 0:
             print("===== GAME OVER =====")
             active = False
-        
-        output_d = display_output(guess, word, display)
 
-        if word == output_d:
+        check = display_output(guess, word, display)
+
+        if word == check:
             print("===== YOU WIN! =====")
             active = False
-
-
+    
 
 if __name__ == "__main__":
     main()
