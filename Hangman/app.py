373dcd040558578643b8325e4d4e2f43ee5c4bf2
thatguykwameC <thatguykwamec@gmail.com>
2026-06-06 18:45:00 -0400

    implemented game_status

--- Hangman/app.py
+++ Hangman/app.py
@@ -1,36 +1,30 @@
-from config import word, display
+from config import MAX_LIVES, word, display
 from utils import (
     welcome_message,
     get_user_input,
     lives_lost,
     display_output,
+    game_status,
 )
 
 
 def main():
     """the main app"""
 
-    life_count = 7
-    count = 0
-    active = True
     guessed = []
+    life_count = MAX_LIVES
 
     welcome_message()
 
-    while active:
+    while True:
         guess = get_user_input(guessed)
 
-        life_count = lives_lost(guess, life_count, count)
-
-        if life_count == 0:
-            print("===== GAME OVER =====")
-            active = False
+        life_count = lives_lost(guess, life_count)
 
         check = display_output(guess, word, display)
 
-        if word == check:
-            print("===== YOU WIN! =====")
-            active = False
+        if not game_status(life_count, word, check):
+            break
 
 
 if __name__ == "__main__":
