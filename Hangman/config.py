814ea73e598b003f49904de0b9c20d253bb37147
thatguykwameC <thatguykwamec@gmail.com>
2026-06-05 21:48:33 -0400

    Pep8 styling

--- Hangman/config.py
+++ Hangman/config.py
@@ -11,67 +11,100 @@ GAME_COVER = r"""
 """
 
 # ASCII art for the gallows
-HANGMANPICS = [r'''
+HANGMANPICS = [
+    r"""
   +---+
   |   |
       |
       |
       |
       |
-=========''', r'''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', r'''
+=========""",
+    r"""
   +---+
   |   |
   O   |
+      |
+      |
+      |
+=========""",
+    r"""
+  +---+
+  |   |
+  O   |
   |   |
       |
       |
-=========''', r'''
+=========""",
+    r"""
   +---+
   |   |
   O   |
  /|   |
       |
       |
-=========''', r'''
+=========""",
+    r"""
   +---+
   |   |
   O   |
  /|\  |
       |
       |
-=========''', r'''
+=========""",
+    r"""
   +---+
   |   |
   O   |
  /|\  |
  /    |
       |
-=========''', r'''
+=========""",
+    r"""
   +---+
   |   |
   O   |
  /|\  |
  / \  |
       |
-=========''']
+=========""",
+]
 
 # animal pool from which an animal is chosen at random
 animal_pool = [
-    "fox", "emu", "pig", "cat", "dog", "ant", "bat", "yak", "owl", "cow",
-    "lion", "frog", "bear", "wolf", "hawk", "crab", "duck", "goat", "mule",
-    "tiger", "shark", "whale", "rhino", "zebra", "chimp", "hyena", "camel", "leopard",
-    "dolphin", "flamingo"
+    "fox",
+    "emu",
+    "pig",
+    "cat",
+    "dog",
+    "ant",
+    "bat",
+    "yak",
+    "owl",
+    "cow",
+    "lion",
+    "frog",
+    "bear",
+    "wolf",
+    "hawk",
+    "crab",
+    "duck",
+    "goat",
+    "mule",
+    "tiger",
+    "shark",
+    "whale",
+    "rhino",
+    "zebra",
+    "chimp",
+    "hyena",
+    "camel",
+    "leopard",
+    "dolphin",
+    "flamingo",
 ]
 
 # variables
 word = choice(animal_pool)
 dashes = "_" * len(word)
 display = list(dashes)
-
