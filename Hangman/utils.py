8e0497fcb5aeb2be0bc6ce071c41c577a18bcb07
thatguykwameC <thatguykwamec@gmail.com>
2026-06-10 19:00:15 -0400

    Added helper function

--- Hangman/utils.py
+++ Hangman/utils.py
@@ -1,16 +1,23 @@
+from random import choice
+
 from config import (
     GAME_COVER,
     HANGMANPICS,
+    ANIMAL_POOL,
     MAX_LIVES,
-    word,
-    dashes,
+    VALID_RESPONSES,
 )
 
 
-def welcome_message(dashes):
+def welcome_message():
     """Displays the game banner"""
     print(GAME_COVER)
-    print(f"Animal to guess: {dashes}")
+
+
+def word_to_be_guessed():
+    """Chooses the word to be guessed"""
+    word = choice(ANIMAL_POOL)
+    return word
 
 
 # Helper function that checks index
@@ -20,6 +27,14 @@ def checks_index(guess, word, display):
         if letter == guess:
             display[index] = guess
     return display
+
+
+def play_again():
+    """Asks if the user wants to play again"""
+    play_again = input("Do you wish to play again? (Y/N) ").strip().lower()
+    if play_again in VALID_RESPONSES:
+        return play_again
+    print(f"Choose (Y/N)")
 
 
 def get_user_input(guessed):
@@ -43,16 +58,17 @@ def get_user_input(guessed):
         return guess
 
 
-def lives_lost(guess, life_count):
+def lives_lost(guess, life_count, word):
     """Handles wrong inputs"""
+    # life_count = MAX_LIVES
     if guess not in word:
-        print(f"You guessed '{guess}'. That is not in the word, you lose a life")
+        print(f"Your guess '{guess}' is not in the word, you lose a life")
         print(HANGMANPICS[MAX_LIVES - life_count])
 
         life_count -= 1
 
         print(
-            f"********************* {life_count}/{MAX_LIVES} LIVES LEFT *********************"
+            f"***************** {life_count}/{MAX_LIVES} LIVES LEFT *****************"
         )
     return life_count
 
@@ -77,4 +93,25 @@ def game_status(life_count, word, check):
         print("===== YOU WIN! =====")
         # print(f"The animal is {word}")
         return False
+
     return True
+
+
+def play(guessed):
+    """Wrapper function"""
+    word = word_to_be_guessed()
+    dashes = "_" * len(word)
+    print(f"Animal to guess: {dashes}")
+    display = list(dashes)
+    game_guessed = guessed[:]
+    life_count = MAX_LIVES
+
+    while True:
+        guess = get_user_input(game_guessed)
+
+        life_counter = lives_lost(guess, life_count, word)
+
+        check = display_output(guess, word, display)
+
+        if not game_status(life_counter, word, check):
+            break
