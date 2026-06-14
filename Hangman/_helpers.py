8eef3da71bb7b7d5d31588d24a68281320efa052
thatguykwameC <thatguykwamec@gmail.com>
2026-06-14 19:22:55 -0400

    Added a helper functions module

---
+++ Hangman/_helpers.py
@@ -0,0 +1,34 @@
+# Helper Functions
+
+
+def _create_display(word):
+    """Replaces letters in word with dashes"""
+    display = []
+
+    for char in word:
+        if char == " ":
+            display.append(" ")
+        else:
+            display.append("_")
+
+    return display
+
+
+def _format_display(display):
+    """Formats the display for user output."""
+    formatted = ""
+
+    for char in display:
+        if char == " ":
+            formatted += "   "
+        else:
+            formatted += f"{char} "
+
+    return formatted.rstrip()
+
+
+def _checks_index(guess, word, display):
+    """Finds all occurrences of guess and updates display accordingly"""
+    for index, letter in enumerate(word):
+        if letter == guess:
+            display[index] = guess
